8324ce8897de9bb416f3eed4fb31a347af263229
jpfnice <jpfnice@gmail.com>
2026-02-06 10:36:00 +0100

    Add files via upload

---
+++ dictType1.py
@@ -0,0 +1,61 @@
+"""
+dict: a mutable collection
+
+    len(), for, in, not in, ... are available because a dict is a "collection"
+"""
+
+# To create an empty dict:
+d1=dict()
+#or
+d1={}
+
+print(d1, type(d1), len(d1))
+
+# To create a dict with a few elements in it:
+d1={
+    "maria":27845676, 
+    "peter":27867899
+}
+
+# print(d1, type(d1), len(d1))
+
+# for k in d1:
+#     print(k, d1[k])
+    
+if "mario" in d1:
+    print(d1["mario"])
+
+print(d1["maria"])
+print(d1.get("maria"))
+
+d1["paul"]=27977666 # I add a new pair to d1
+d1["maria"]=27977665 # I update the value associated with the key Maria
+
+print(d1["maria"])
+
+print(d1, type(d1), len(d1))
+
+d1={
+         10:["raphael", "gabriel", "thomas"], 
+         11:["julia", "yann"]
+}
+
+print(d1, type(d1), len(d1))
+
+print(d1[10])
+
+# print(d1.get(10))
+
+d1[9]=["mario"]
+
+print(d1, type(d1), len(d1))
+
+for k in d1:
+    print(k,"->",d1[k])
+    
+d1[9].append("julia")
+
+for k in d1:
+    print(k,"->",d1[k])
+
+
---
+++ dictType2.py
@@ -0,0 +1,35 @@
+d1={
+    "maria":27845676, 
+    "peter":27867899
+}
+
+d1["paul"]=27977666
+
+# for e in d1:
+#     print(e, "->", d1[e])
+
+# for e in d1.items():
+#     #print(e)
+#     print(e[0],"->", e[1])
+    
+# for k,v in d1.items(): # unpacking
+#     print(k,"->", v)
+
+for v in d1.values():
+    print(v)
+
+
+for k in d1.keys():
+    print(k)
+    
+d1.pop("paul")
+
+print()
+
+for k in d1.keys():
+    print(k)
+    
+# if "peter" in d1:
+#     d1.pop("peter")
+
+# print(d1)
