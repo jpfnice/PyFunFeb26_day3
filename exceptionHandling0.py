9a1f838dda82d21c4a7874d65914a09742c25b8a
jpfnice <jpfnice@gmail.com>
2026-02-06 08:14:51 +0100

    Add files via upload

---
+++ exceptionHandling0.py
@@ -0,0 +1,7 @@
+data=[34,56,78,89,10,30]
+
+index=input("Pleaser enter an int in the range [0,5]: ")
+index=int(index) # may raise a ValueError
+print("At position", index, "there is the element", data[index]) # may raise an IndexError 
+    
+print("The end")
